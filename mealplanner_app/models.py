da2a20adb9a1774eb94f27ab383b6ebc1b7d0aca
Fikri Aqsha <m.fikriaqsha@gmail.com>
2023-08-31 11:14:25 +0700

    create models

--- mealplanner_app/models.py
+++ mealplanner_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Week(models.Model):
+    week = models.CharField(max_length=10)
+        
+    def __str__(self) -> str:
+        return self.week
+
+class Day(models.Model):
+    day = models.CharField(max_length=20)
+    week = models.ForeignKey(Week, on_delete=models.PROTECT)
+    
+    def __str__(self) -> str:
+        return self.day
+
+class Meal(models.Model):
+    meal = models.TextField()
+    day = models.ForeignKey(Day, on_delete=models.PROTECT)
+    week = models.ForeignKey(Week, on_delete=models.PROTECT)
+    
+    class Meta:
+        verbose_name_plural = "meals"
+    
+    def __str__(self) -> str:
+        return self.meal
